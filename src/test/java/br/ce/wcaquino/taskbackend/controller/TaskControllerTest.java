d7ed6373daf583271b69ff4f6590cf237cf607dc
Geraldo <gerafenster@gmail.com>
2020-06-24 15:54:28 -0300

    Mais commit

--- src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -33,7 +33,7 @@ public class TaskControllerTest {
 		todo.setDueDate(LocalDate.now());
 		try {
 			controller.save(todo);
-			Assert.fail("Não deveria chegar nesse ponto.")
+			Assert.fail("Não deveria chegar nesse ponto.");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Fill the task description", e.getMessage());
 		}
